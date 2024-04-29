4c2c4c62051966f2163c132204681c4fa03b036f
xiangnan <xiangnan.wjfjdw@gmail.com>
2024-04-30 01:18:18 +0800

    整理data目录里

--- scripts/main.py
+++ scripts/main.py
@@ -17,10 +17,10 @@ pd.set_option('display.width', None)  # 设置宽度不限制
 def run_china_market():
     csv_config_dict = dict(
         futures_single_contracts_dir='data/china_futures/single_contracts',
-        futures_continuous_dir='data/output/backtest2/continuous',
-        futures_combined_dir='data/output/backtest2/combined',
-        futures_roll_calendar_dir='data/output/backtest2/roll_calendar',
-        daily_account_value_file_path='data/output/backtest2/daily_account_value.csv',
+        futures_continuous_dir='data/china_futures/continuous',
+        futures_combined_dir='data/china_futures/combined',
+        futures_roll_calendar_dir='data/china_futures/roll_calendar',
+        daily_account_value_file_path='data/china_futures/daily_account_value.csv',
         portfolio_config_file_path='config/symbols_china_futures.csv',
         strategy_rules_config_file_path='config/strategy_rules_china.csv',
         strategy_parameters_config_file_path='config/strategy_parameters_china.csv',
@@ -38,10 +38,10 @@ def run_china_market():
 def run_us_market():
     csv_config_dict = dict(
         futures_single_contracts_dir='data/futures/single_contracts',
-        futures_continuous_dir='data/output/us_ewmac/continuous',
-        futures_combined_dir='data/output/us_ewmac/combined',
-        futures_roll_calendar_dir='data/output/us_ewmac/roll_calendar',
-        daily_account_value_file_path='data/output/us_ewmac/daily_account_value.csv',
+        futures_continuous_dir='data/futures/continuous',
+        futures_combined_dir='data/futures/combined',
+        futures_roll_calendar_dir='data/futures/roll_calendar',
+        daily_account_value_file_path='data/futures/daily_account_value.csv',
         portfolio_config_file_path='config/symbols_us_futures.csv',
         strategy_rules_config_file_path='config/strategy_rules.csv',
         strategy_parameters_config_file_path='config/strategy_parameters.csv',
